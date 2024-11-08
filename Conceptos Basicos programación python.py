f25d23ffce4ce1c26baecc6e4868b36e0c7c07af
Abel Herrera <abel.herrera.montiel@outlook.com>
2024-11-07 21:24:34 -0500

    Conceptos basicos de python Sintaxis, Semántica y Variables

---
+++ Conceptos Basicos programación python.py
@@ -0,0 +1,17 @@
+print("hola mundo")
+
+print("hola")
+print("mundo")
+#error de identación o sangría que son espacios adicionales al inicio de cada linea.
+#print("hola")
+#   print("mundo")
+
+#Variables: Contenedores de diferentes datos de información tanto numéricos como de texto. Las variables constan de 3 partes Nombre, Tipo y Dato. 
+#diaCumpleaños = init(10) 
+#no se puede usar números al inicio de los nombres de las variables tampoco usar palabrar reservadas.
+
+saludo_1 = "Hola"
+saludo_2 = "Hi"
+nombre = "Abel"
+print(saludo_2)
+print(nombre)
