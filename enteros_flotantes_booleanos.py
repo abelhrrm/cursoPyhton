e8955a0073fe760702b59e27d418122eab1179e6
Abel Herrera <abel.herrera.montiel@outlook.com>
2024-11-09 21:04:35 -0500

    Tipos de números enteros, flotantes y booleanos en python.

---
+++ enteros_flotantes_booleanos.py
@@ -0,0 +1,25 @@
+#En python podemos tratar tres tipos de números, enteros, flotantes y booleanos:
+x = 10
+y = 5.8378363
+print(type(x))
+print(type(y))
+#También tenemos los números de notación científica esto se realiza con la letra "e"
+z_1 = 20.8e7
+z_2 = 20.8e-7
+print(type(z_1))
+print(type(z_2))
+print(z_1)
+print(z_2)
+
+#Operaciones suma:
+print(x + x)
+print(x + y) #La operación entre un entero y un flotante termina como resultado un flotante.
+print(y + y)
+
+#Boleanos la sintaxis es con mayúscula inicial "True" y "False"
+is_true = True
+is_false = False
+print(type(is_true))
+print(is_true)
+print(is_false)
+
